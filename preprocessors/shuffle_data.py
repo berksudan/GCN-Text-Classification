4e6265085a87139966ad7bcec7e9f8c6b454d7d2
Berk Sudan <berk.sudan.96@gmail.com>
2020-05-27 01:05:17 +0300

    refactored and fixed small bugs

--- preprocessors/shuffle_data.py
+++ preprocessors/shuffle_data.py
@@ -2,7 +2,7 @@ import random
 from typing import List, Tuple
 
 from common import check_data_set
-from preprocessors.preprocessing_configs import PreProcessingConfigs
+from preprocessors.configs import PreProcessingConfigs
 from utils.file_ops import create_dir, write_iterable_to_file, check_paths
 
 
@@ -14,22 +14,22 @@ def load_corpus_meta(corpus_meta_path: str) -> Tuple[List[str], List[str], List[
 
 
 def shuffle_data(ds_name: str, cfg: PreProcessingConfigs):
-    ds_corpus = cfg.CORPUS_CLEANED_DIR + ds_name + cfg.DATA_SET_EXTENSION
-    ds_corpus_meta = cfg.CORPUS_META_DIR + ds_name + '.meta'
+    ds_corpus = cfg.corpus_cleaned_dir + ds_name + cfg.data_set_extension
+    ds_corpus_meta = cfg.corpus_meta_dir + ds_name + '.meta'
 
-    ds_corpus_shuffled = cfg.CORPUS_SHUFFLED_DIR + ds_name + cfg.DATA_SET_EXTENSION
-    ds_corpus_shuffled_train_idx = cfg.CORPUS_SHUFFLED_SPLIT_INDEX_DIR + ds_name + '.train'
-    ds_corpus_shuffled_test_idx = cfg.CORPUS_SHUFFLED_SPLIT_INDEX_DIR + ds_name + '.test'
-    ds_corpus_shuffled_meta = cfg.CORPUS_SHUFFLED_META_DIR + ds_name + '.meta'
+    ds_corpus_shuffled = cfg.corpus_shuffled_dir + ds_name + cfg.data_set_extension
+    ds_corpus_shuffled_train_idx = cfg.corpus_shuffled_split_index_dir + ds_name + '.train'
+    ds_corpus_shuffled_test_idx = cfg.corpus_shuffled_split_index_dir + ds_name + '.test'
+    ds_corpus_shuffled_meta = cfg.corpus_shuffled_meta_dir + ds_name + '.meta'
 
     # Checkers
-    check_data_set(data_set_name=ds_name, all_data_set_names=cfg.DATA_SETS)
+    check_data_set(data_set_name=ds_name, all_data_set_names=cfg.data_sets)
     check_paths(ds_corpus_meta, ds_corpus)
 
     # Create dirs if not exist
-    create_dir(cfg.CORPUS_SHUFFLED_DIR, overwrite=False)
-    create_dir(cfg.CORPUS_SHUFFLED_META_DIR, overwrite=False)
-    create_dir(cfg.CORPUS_SHUFFLED_SPLIT_INDEX_DIR, overwrite=False)
+    create_dir(cfg.corpus_shuffled_dir, overwrite=False)
+    create_dir(cfg.corpus_shuffled_meta_dir, overwrite=False)
+    create_dir(cfg.corpus_shuffled_split_index_dir, overwrite=False)
 
     all_doc_meta_list, train_doc_meta_list, test_doc_meta_list = load_corpus_meta(corpus_meta_path=ds_corpus_meta)
     cleaned_doc_lines = [line.strip() for line in open(ds_corpus, 'r')]
@@ -53,18 +53,5 @@ def shuffle_data(ds_name: str, cfg: PreProcessingConfigs):
     shuffled_doc_lines = [cleaned_doc_lines[all_doc_meta_id] for all_doc_meta_id in all_doc_meta_ids]
     write_iterable_to_file(an_iterable=shuffled_doc_lines, file_path=ds_corpus_shuffled, file_mode='w')
 
-    print("[INFO] Shuffled-Corpus Dir='{}'".format(cfg.CORPUS_SHUFFLED_DIR))
+    print("[INFO] Shuffled-Corpus Dir='{}'".format(cfg.corpus_shuffled_dir))
     print("[INFO] ========= SHUFFLED DATA: Corpus documents shuffled. =========")
-#
-# if __name__ == '__main__':
-# # Pre-Defined Parameters
-# # DATA_SETS = ['20ng', 'R8', 'R52', 'ohsumed', 'mr']
-# # CORPUS_DIR = '../data/corpus/'
-# # CORPUS_META_DIR = '../data/corpus/meta/'
-# # CORPUS_CLEANED_DIR = '../data/corpus.cleaned/'
-# # CORPUS_SHUFFLED_DIR = '../data/corpus.shuffled/'
-# # CORPUS_SHUFFLED_SPLIT_INDEX_DIR = '../data/corpus.shuffled/split_index/'
-# # CORPUS_SHUFFLED_META_DIR = '../data/corpus.shuffled/meta/'
-#
-# # for data_set in DATA_SETS:
-# #     shuffle_data(ds_name=data_set)
