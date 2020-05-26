baaca75142eff90ba46d93642fceb55fbf299673
bs <23002942+berksudan@users.noreply.github.com>
2020-05-27 01:11:20 +0300

    preprocess enhanced

--- common.py
+++ common.py
@@ -1,4 +1,5 @@
 from collections import Counter
+from typing import Iterable, Any
 from typing import List
 
 
@@ -14,4 +15,5 @@ def check_data_set(data_set_name: str, all_data_set_names: List[str]) -> None:
         raise AttributeError("Wrong data-set name, given:%r, however expected:%r" % (data_set_name, all_data_set_names))
 
 
-
+def flatten_nested_iterables(iterables_of_iterables: Iterable[Iterable[Any]]) -> Iterable[Any]:
+    return [item for sublist in iterables_of_iterables for item in sublist]
--- preprocess.py
+++ preprocess.py
@@ -1,32 +1,29 @@
 from preprocessors.build_adjacency import build_adjacency
 from preprocessors.build_node_features import build_node_features
 from preprocessors.clean_data import clean_data
+from preprocessors.configs import PreProcessingConfigs
 from preprocessors.prepare_words import prepare_words
-from preprocessors.preprocessing_configs import PreProcessingConfigs
 from preprocessors.shuffle_data import shuffle_data
 
-prep_configs = PreProcessingConfigs()
-prep_configs.DATA_SETS = ['20ng', 'R8', 'R52', 'ohsumed', 'mr']
-prep_configs.DATA_SET_EXTENSION = '.txt'
+# Create pre-processing configs
+conf = PreProcessingConfigs()
+conf.data_sets = ['20ng', 'R8', 'R52', 'ohsumed', 'mr', 'cora']
+conf.data_set_extension = '.txt'
+conf.corpus_dir = 'data/corpus/'
+conf.corpus_meta_dir = 'data/corpus/meta/'
+conf.corpus_cleaned_dir = 'data/corpus.cleaned/'
+conf.corpus_shuffled_dir = 'data/corpus.shuffled/'
+conf.corpus_shuffled_split_index_dir = 'data/corpus.shuffled/split_index/'
+conf.corpus_shuffled_meta_dir = 'data/corpus.shuffled/meta/'
+conf.corpus_shuffled_vocab_dir = 'data/corpus.shuffled/vocabulary/'
+conf.corpus_shuffled_word_vectors_dir = 'data/corpus.shuffled/word_vectors/'
+conf.corpus_shuffled_adjacency_dir = 'data/corpus.shuffled/adjacency/'
+conf.corpus_shuffled_node_features_dir = 'data/corpus.shuffled/node_features/'
+conf.build()
 
-prep_configs.CORPUS_DIR = 'data/corpus/'
-prep_configs.CORPUS_META_DIR = 'data/corpus/meta/'
-
-prep_configs.CORPUS_CLEANED_DIR = 'data/corpus.cleaned/'
-
-prep_configs.CORPUS_SHUFFLED_DIR = 'data/corpus.shuffled/'
-prep_configs.CORPUS_SHUFFLED_SPLIT_INDEX_DIR = 'data/corpus.shuffled/split_index/'
-prep_configs.CORPUS_SHUFFLED_META_DIR = 'data/corpus.shuffled/meta/'
-
-prep_configs.CORPUS_SHUFFLED_VOCAB_DIR = 'data/corpus.shuffled/vocabulary/'
-prep_configs.CORPUS_SHUFFLED_WORD_VECTORS_DIR = 'data/corpus.shuffled/word_vectors/'
-prep_configs.CORPUS_SHUFFLED_ADJACENCY_DIR = 'data/corpus.shuffled/adjacency/'
-
-prep_configs.CORPUS_SHUFFLED_NODE_FEATURES_DIR = 'data/corpus.shuffled/node_features/'
-prep_configs.build()
-
-clean_data(ds_name='R52', rare_count=5, cfg=prep_configs)
-shuffle_data(ds_name='R52', cfg=prep_configs)
-prepare_words(ds_name='R52', cfg=prep_configs)
-build_node_features(ds_name='R52', train_ratio=0.90, use_predefined_word_vectors=False, cfg=prep_configs)
-build_adjacency(ds_name='R52', cfg=prep_configs)
+# Start pre-processing
+clean_data(ds_name='R52', rare_count=5, cfg=conf)
+shuffle_data(ds_name='R52', cfg=conf)
+prepare_words(ds_name='R52', cfg=conf)
+build_node_features(ds_name='R52', validation_ratio=0.10, use_predefined_word_vectors=False, cfg=conf)
+build_adjacency(ds_name='R52', cfg=conf)
