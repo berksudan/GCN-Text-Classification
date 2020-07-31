63196e3b25f128a4d1f7f4ba4172dc906ce9e1cd
Berk Sudan <berk.sudan.96@gmail.com>
2020-07-31 23:29:28 +0300

    datasets are now given as parameters from console

--- preprocess.py
+++ preprocess.py
@@ -1,3 +1,5 @@
+from sys import argv
+
 from preprocessors.build_adjacency import build_adjacency
 from preprocessors.build_node_features import build_node_features
 from preprocessors.clean_data import clean_data
@@ -5,25 +7,36 @@ from preprocessors.configs import PreProcessingConfigs
 from preprocessors.prepare_words import prepare_words
 from preprocessors.shuffle_data import shuffle_data
 
-# Create pre-processing configs
-conf = PreProcessingConfigs()
-conf.data_sets = ['20ng', 'R8', 'R52', 'ohsumed', 'mr', 'cora', 'citeseer', 'pubmed']
-conf.data_set_extension = '.txt'
-conf.corpus_dir = 'data/corpus/'
-conf.corpus_meta_dir = 'data/corpus/meta/'
-conf.corpus_cleaned_dir = 'data/corpus.cleaned/'
-conf.corpus_shuffled_dir = 'data/corpus.shuffled/'
-conf.corpus_shuffled_split_index_dir = 'data/corpus.shuffled/split_index/'
-conf.corpus_shuffled_meta_dir = 'data/corpus.shuffled/meta/'
-conf.corpus_shuffled_vocab_dir = 'data/corpus.shuffled/vocabulary/'
-conf.corpus_shuffled_word_vectors_dir = 'data/corpus.shuffled/word_vectors/'
-conf.corpus_shuffled_adjacency_dir = 'data/corpus.shuffled/adjacency/'
-conf.corpus_shuffled_node_features_dir = 'data/corpus.shuffled/node_features/'
-conf.build()
 
-# Start pre-processing
-clean_data(ds_name='citeseer', rare_count=5, cfg=conf)
-shuffle_data(ds_name='citeseer', cfg=conf)
-prepare_words(ds_name='citeseer', cfg=conf)
-build_node_features(ds_name='citeseer', validation_ratio=0.10, use_predefined_word_vectors=False, cfg=conf)
-build_adjacency(ds_name='citeseer', cfg=conf)
+def create_preprocessing_cfg() -> PreProcessingConfigs:
+    conf = PreProcessingConfigs()
+    conf.data_sets = ['20ng', 'R8', 'R52', 'ohsumed', 'mr', 'cora', 'citeseer', 'pubmed']
+    conf.data_set_extension = '.txt'
+    conf.corpus_dir = 'data/corpus/'
+    conf.corpus_meta_dir = 'data/corpus/meta/'
+    conf.corpus_cleaned_dir = 'data/corpus.cleaned/'
+    conf.corpus_shuffled_dir = 'data/corpus.shuffled/'
+    conf.corpus_shuffled_split_index_dir = 'data/corpus.shuffled/split_index/'
+    conf.corpus_shuffled_meta_dir = 'data/corpus.shuffled/meta/'
+    conf.corpus_shuffled_vocab_dir = 'data/corpus.shuffled/vocabulary/'
+    conf.corpus_shuffled_word_vectors_dir = 'data/corpus.shuffled/word_vectors/'
+    conf.corpus_shuffled_adjacency_dir = 'data/corpus.shuffled/adjacency/'
+    conf.corpus_shuffled_node_features_dir = 'data/corpus.shuffled/node_features/'
+    conf.build()
+    return conf
+
+
+def preprocess(ds: str, preprocessing_cfg: PreProcessingConfigs):  # Start pre-processing
+    clean_data(ds_name=ds, rare_count=5, cfg=preprocessing_cfg)
+    shuffle_data(ds_name=ds, cfg=preprocessing_cfg)
+    prepare_words(ds_name=ds, cfg=preprocessing_cfg)
+    build_node_features(ds_name=ds, validation_ratio=0.10, use_predefined_word_vectors=False, cfg=preprocessing_cfg)
+    build_adjacency(ds_name=ds, cfg=preprocessing_cfg)
+
+
+if __name__ == '__main__':
+    prep_cfg = create_preprocessing_cfg()
+    if len(argv) < 2:
+        raise Exception("Dataset name cannot be left blank. Must be one of datasets:%r." % prep_cfg.data_sets)
+    ds_name = argv[1]
+    preprocess(ds=ds_name, preprocessing_cfg=prep_cfg)
--- train.py
+++ train.py
@@ -1,22 +1,36 @@
+from sys import argv
+
 from trainer.configs import TrainingConfigs
 from trainer.train_model import train_model
 
-# Create training configs
-conf = TrainingConfigs()
-conf.data_sets = ['20ng', 'R8', 'R52', 'ohsumed', 'mr', 'cora', 'citeseer', 'pubmed']
-conf.corpus_split_index_dir = 'data/corpus.shuffled/split_index/'
-conf.corpus_node_features_dir = 'data/corpus.shuffled/node_features/'
-conf.corpus_adjacency_dir = 'data/corpus.shuffled/adjacency/'
-conf.corpus_vocab_dir = 'data/corpus.shuffled/vocabulary/'
-conf.model = 'gcn'
-conf.learning_rate = 0.02
-conf.epochs = 200
-conf.hidden1 = 200
-conf.dropout = 0.5
-conf.weight_decay = 0.
-conf.early_stopping = 10
-conf.chebyshev_max_degree = 3
-conf.build()
 
-# Start training
-train_model(ds_name='citeseer', is_featureless=True, cfg=conf)
+def create_training_cfg() -> TrainingConfigs:
+    conf = TrainingConfigs()
+    conf.data_sets = ['20ng', 'R8', 'R52', 'ohsumed', 'mr', 'cora', 'citeseer', 'pubmed']
+    conf.corpus_split_index_dir = 'data/corpus.shuffled/split_index/'
+    conf.corpus_node_features_dir = 'data/corpus.shuffled/node_features/'
+    conf.corpus_adjacency_dir = 'data/corpus.shuffled/adjacency/'
+    conf.corpus_vocab_dir = 'data/corpus.shuffled/vocabulary/'
+    conf.model = 'gcn'
+    conf.learning_rate = 0.02
+    conf.epochs = 200
+    conf.hidden1 = 200
+    conf.dropout = 0.5
+    conf.weight_decay = 0.
+    conf.early_stopping = 10
+    conf.chebyshev_max_degree = 3
+    conf.build()
+    return conf
+
+
+def train(ds: str, training_cfg: TrainingConfigs):
+    # Start training
+    train_model(ds_name=ds, is_featureless=True, cfg=training_cfg)
+
+
+if __name__ == '__main__':
+    trn_cfg = create_training_cfg()
+    if len(argv) < 2:
+        raise Exception("Dataset name cannot be left blank. Must be one of datasets:%r." % trn_cfg.data_sets)
+    ds_name = argv[1]
+    train(ds=ds_name, training_cfg=trn_cfg)
